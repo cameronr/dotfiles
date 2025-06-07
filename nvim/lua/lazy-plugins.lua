ad989733ba6f35ca4cbbcb3ecef9749900c62fe4
Cameron Ring <cameron@cs.stanford.edu>
2025-06-07 14:17:46 -0700

    fix: lazy load guess indent
    also include commented out disabling compression plugins

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -11,7 +11,10 @@
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  {
+    'NMAC427/guess-indent.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+  }, -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -101,6 +104,10 @@ require('lazy').setup({
       disabled_plugins = {
         -- 'matchparen',
         -- "netrwPlugin",
+        -- 'gzip',
+        -- 'tarPlugin',
+        -- 'zipPlugin',
+        -- 'tohtml',
       },
     },
   },
