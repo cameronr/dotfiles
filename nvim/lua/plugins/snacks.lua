1cc102f4b4086cdf57a285949eef46df0a68de7e
Cameron Ring <cameron@cs.stanford.edu>
2025-06-08 17:22:01 -0700

    feat(snacks): enable backdrop

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -94,6 +94,9 @@ return {
           cycle = true,
           --- Use the default layout or vertical if the window is too narrow
           preset = function() return vim.o.columns >= 120 and 'telescope' or 'vertical' end,
+          layout = {
+            backdrop = true,
+          },
         },
         win = {
           -- input window
