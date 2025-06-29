48ff981d8d184cda85d62f3e914e7060ddcf047e
Cameron Ring <cameron@cs.stanford.edu>
2025-06-28 19:30:37 -0700

    chore: removing unused plugins

--- nvim/lua/plugins/helpview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'OXY2DEV/helpview.nvim',
-  enabled = false,
-  -- lazy = false, -- Recommended
-  ft = 'help',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-  },
-}
--- nvim/lua/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'mbbill/undotree',
-  enabled = false,
-
-  keys = {
-    { '<leader>u', '<cmd>UndotreeToggle<CR>', desc = 'Undotree' },
-  },
-}
