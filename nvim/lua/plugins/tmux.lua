98f389d2655d56729353f5dc0448c7a899980ef7
Cameron Ring <cameron@cs.stanford.edu>
2025-06-30 21:23:41 -0700

    feat(tmux): use A-hjkl to resize windows

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -5,6 +5,10 @@ return {
     { '<C-j>', [[<cmd>lua require("tmux").move_bottom()<cr>]] },
     { '<C-k>', [[<cmd>lua require("tmux").move_top()<cr>]] },
     { '<C-l>', [[<cmd>lua require("tmux").move_right()<cr>]] },
+    { '<A-h>', [[<cmd>lua require("tmux").move_left()<cr>]] },
+    { '<A-j>', [[<cmd>lua require("tmux").move_bottom()<cr>]] },
+    { '<A-k>', [[<cmd>lua require("tmux").move_top()<cr>]] },
+    { '<A-l>', [[<cmd>lua require("tmux").move_right()<cr>]] },
     -- { '<C-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
   },
   opts = {
@@ -14,6 +18,16 @@ return {
     },
     navigation = {
       cycle_navigation = false,
+    },
+    resize = {
+      -- enables default keybindings (A-hjkl) for normal mode
+      enable_default_keybindings = true,
+
+      -- sets resize steps for x axis
+      resize_step_x = 2,
+
+      -- sets resize steps for y axis
+      resize_step_y = 2,
     },
   },
 }
