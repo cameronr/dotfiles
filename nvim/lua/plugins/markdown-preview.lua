c8b274e1ef5be964dc41837a661b4c0d61c71604
Cameron Ring <cameron@cs.stanford.edu>
2025-07-11 14:15:37 -0700

    fix(markdown-preview): test for yarn

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -4,5 +4,6 @@ return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   build = 'cd app && yarn install',
+  cond = function() return vim.fn.executable('yarn') == 1 end,
   init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
 }
