34dc46ac8ad134ca2a9c6f44019b75b1dfa0e3ac
Cameron Ring <cameron@cs.stanford.edu>
2025-07-13 15:49:45 -0700

    gha

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,7 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- test change 5
+-- test change 6
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
