fbc1f0947e1002eaaa016a0743d1606c1549aea2
Cameron Ring <cameron@cs.stanford.edu>
2025-07-13 16:07:15 -0700

    chore: remove debugging comment

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,6 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- test change
-
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR><esc>')
