f8885f62519718256b31a4c77227337690107186
Cameron Ring <cameron@cs.stanford.edu>
2025-08-10 08:07:48 -0700

    feat(autocmds): create missing directories on save

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -33,3 +33,13 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
     if vim.o.filetype == 'help' then vim.cmd.wincmd('L') end
   end,
 })
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = user_autocmds_augroup,
+  callback = function(event)
+    if event.match:match('^%w%w+:[\\/][\\/]') then return end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
