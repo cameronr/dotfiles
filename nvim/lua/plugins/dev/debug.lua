44df083d47bad143e54ae746942f85bf0a44f034
Cameron Ring <cameron@cs.stanford.edu>
2025-08-14 10:28:03 -0700

    fix(debug): add cast annotation

--- nvim/lua/plugins/dev/debug.lua
+++ nvim/lua/plugins/dev/debug.lua
@@ -63,6 +63,7 @@ return {
 
       for name, sign in pairs(require('globals').icons.dap) do
         sign = type(sign) == 'table' and sign or { sign }
+        ---@cast sign table
         vim.fn.sign_define('Dap' .. name, { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
       end
 
