b1848c73f05e4d4160a08c2885b2ea8d16b2541e
Cameron Ring <cameron@cs.stanford.edu>
2025-08-14 23:13:42 -0700

    chore(markdown-preview): remove

--- nvim/lua/plugins/dev/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
----@module 'lazy'
----@type LazySpec
-return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && yarn install',
-  cond = function() return vim.fn.executable('yarn') == 1 end,
-  init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
-}
