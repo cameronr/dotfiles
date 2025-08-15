ccc59ea77d19e8bf77cc605651f536bfd45359fb
Cameron Ring <cameron@cs.stanford.edu>
2025-08-14 23:15:16 -0700

    feat(rust_analyzer): adding options
    I don't use rust much so don't know if these are necessary but seems
    better than losing them

--- nvim/after/lsp/rust_analyzer.lua
+++ nvim/after/lsp/rust_analyzer.lua
@@ -1,4 +1,24 @@
 return {
+  check = {
+    command = 'clippy',
+    extraArgs = { '--no-deps' },
+  },
+  inlayHints = {
+    bindingModeHints = { enable = true },
+    closingBraceHints = { minLines = 0 },
+    closureCaptureHints = { enable = true },
+    closureReturnTypeHints = { enable = 'always' },
+    expressionAdjustmentHints = {
+      enable = 'reborrow',
+      hideOutsideUnsafe = true,
+    },
+    lifetimeElisionHints = {
+      enable = 'skip_trivial',
+      useParameterNames = true,
+    },
+    maxLength = vim.NIL,
+    typing = { triggerChars = '=.{(><' },
+  },
   cargo = {
     allFeatures = true,
     loadOutDirsFromCheck = true,
