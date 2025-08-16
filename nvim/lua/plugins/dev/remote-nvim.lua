ceb84afdfb07c9c4701106aab52fac76a0a7ce7c
Cameron Ring <cameron@cs.stanford.edu>
2025-08-15 23:52:42 -0700

    chore(remote-nvim): remove

--- nvim/lua/plugins/dev/remote-nvim.lua
+++
@@ -1,8 +0,0 @@
----@module 'lazy'
----@type LazySpec
-return {
-  'amitds1997/remote-nvim.nvim',
-  version = '*', -- Pin to GitHub releases
-  cmd = { 'RemoteStart', 'RemoteStop', 'RemoteInfo', 'RemoteCleanup', 'RemoteConfigDel', 'RemoteLog' },
-  config = true,
-}
