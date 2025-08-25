dc7379a0d8f10b8ad94b94b2a66b488bf33bf359
Cameron Ring <cameron@cs.stanford.edu>
2025-08-25 13:16:16 -0700

    feat(nvim-treesitter-context): add toggle

--- nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/lua/plugins/nvim-treesitter-context.lua
@@ -3,8 +3,25 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
   event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    max_lines = 10,
-    multiline_threshold = 3,
-  },
+  opts = function(_, opts)
+    if Snacks then
+      local ts_context = require('treesitter-context')
+      Snacks.toggle({
+        name = 'treesitter context',
+        get = function() return ts_context.enabled() end,
+        set = function(state)
+          if state then
+            ts_context.enable()
+          else
+            ts_context.disable()
+          end
+        end,
+      }):map('<leader>vo')
+    end
+
+    return vim.tbl_deep_extend('force', opts or {}, {
+      max_lines = 10,
+      multiline_threshold = 3,
+    })
+  end,
 }
