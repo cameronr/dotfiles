9bc549a6fe19d561f40647e9738edf05b6112a4a
Cameron Ring <cameron@cs.stanford.edu>
2025-08-29 11:38:29 -0700

    fix(gitsigns): make blame line easier to read

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -11,6 +11,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
 
     opts = {
+      current_line_blame_formatter = '-> <author>, <author_time:%R> - <summary>',
       preview_config = {
         border = 'rounded',
       },
