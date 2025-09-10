4f8163699fb06d5abd5f42cfcd111de533cf77b0
Cameron Ring <cameron@cs.stanford.edu>
2025-09-09 19:57:59 -0700

    fix(clangd): mostly fix c files in diffview warnings

--- nvim/after/lsp/clangd.lua
+++ nvim/after/lsp/clangd.lua
@@ -10,8 +10,16 @@ return {
   },
   ---@param _ vim.lsp.Client
   ---@param bufnr integer
-  on_attach = function(_, bufnr)
+  on_attach = function(client, bufnr)
     local clangd_lspconfig = require('lspconfig.configs.clangd')
+
+    -- Trying to fix error popups with Diffview:// buffers
+    -- Still get an initial error popup but at least it's only the one
+    local uri = vim.uri_from_bufnr(bufnr)
+    if not vim.startswith(uri, 'file://') then
+      vim.schedule(function() vim.lsp.buf_detach_client(bufnr, client.id) end)
+      return
+    end
 
     -- Register clangd commands manually because we're overriding on_attach
     for name, def in pairs(clangd_lspconfig.commands) do
