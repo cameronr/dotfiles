113ef57958919fac623039b3ac77a509bde9fd15
Cameron Ring <cameron@cs.stanford.edu>
2025-09-13 10:37:17 -0700

    feat(rust): lazyvim's rustacean config

--- nvim/lua/plugins/dev/rust.lua
+++ nvim/lua/plugins/dev/rust.lua
@@ -2,7 +2,60 @@ if vim.fn.executable('rustc') ~= 1 then return {} end
 
 return {
   {
-    'neovim/nvim-lspconfig',
-    opts = { servers = { 'rust_analyzer' } },
+    'mrcjkb/rustaceanvim',
+    version = vim.fn.has('nvim-0.10.0') == 0 and '^4' or false,
+    ft = { 'rust' },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set('n', '<leader>cR', function() vim.cmd.RustLsp('codeAction') end, { desc = 'Code Action', buffer = bufnr })
+          vim.keymap.set('n', '<leader>dr', function() vim.cmd.RustLsp('debuggables') end, { desc = 'Rust Debuggables', buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            -- Add clippy lints for Rust if using rust-analyzer
+            checkOnSave = diagnostics == 'rust-analyzer',
+            -- Enable diagnostics if using rust-analyzer
+            diagnostics = {
+              enable = diagnostics == 'rust-analyzer',
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ['async-trait'] = { 'async_trait' },
+                ['napi-derive'] = { 'napi' },
+                ['async-recursion'] = { 'async_recursion' },
+              },
+            },
+            files = {
+              excludeDirs = {
+                '.direnv',
+                '.git',
+                '.github',
+                '.gitlab',
+                'bin',
+                'node_modules',
+                'target',
+                'venv',
+                '.venv',
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function()
+      if vim.fn.executable('rust-analyzer') == 0 then
+        vim.notify('**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/', { title = 'rustaceanvim' })
+      end
+    end,
   },
 }
