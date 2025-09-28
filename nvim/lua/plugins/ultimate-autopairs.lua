ec12fac588d861c3eef38bd096233beea37bd8b6
Cameron Ring <cameron@cs.stanford.edu>
2025-09-27 22:06:02 -0700

    feat(ultimate-autopairs): trying out

---
+++ nvim/lua/plugins/ultimate-autopairs.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'altermo/ultimate-autopair.nvim',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    opts = {
+      bs = {
+        enable = false,
+      },
+      --Config goes here
+    },
+  },
+}
