cd27a6f6de1c775a54ec16aeac3af04f11ad33a3
Cameron Ring <cameron@cs.stanford.edu>
2025-10-03 20:27:37 -0700

    fix(fyler): - for parent, use native toggle

--- nvim/lua/plugins/extra/fyler.lua
+++ nvim/lua/plugins/extra/fyler.lua
@@ -1,19 +1,17 @@
 return {
   'A7Lavinraj/fyler.nvim',
   keys = {
-
     {
       '<Bslash><Bslash>',
-      function()
-        for _, win in ipairs(vim.api.nvim_list_wins()) do
-          if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == 'fyler' then return vim.api.nvim_win_close(win, false) end
-        end
-        require('fyler').open({ kind = 'split_left_most' })
-      end,
+      function() require('fyler').toggle({ kind = 'split_left_most' }) end,
       desc = 'Fyler',
     },
   },
   ---@module 'fyler'
   ---@type FylerSetupOptions
-  opts = {},
+  opts = {
+    mappings = {
+      ['-'] = 'GotoParent',
+    },
+  },
 }
