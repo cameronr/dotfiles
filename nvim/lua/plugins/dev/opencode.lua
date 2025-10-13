aeed17e25e69334787d3253d2cf9508d9a731f53
Cameron Ring <cameron@cs.stanford.edu>
2025-10-12 19:32:18 -0700

    feat(opencode): overhaul
    update to new keymap format, default keymap to leader a, custom keymaps
    
    enable debugging and event capture
    
    add markview support (but currently disabled)

--- nvim/lua/plugins/dev/opencode.lua
+++ nvim/lua/plugins/dev/opencode.lua
@@ -19,20 +19,28 @@ end
 return {
   {
     'sudo-tee/opencode.nvim',
+    -- dev = true,
     cond = function() return vim.fn.executable('opencode') == 1 end,
     cmd = 'Opencode',
     keys = {
       {
-        '<leader>ao',
+        '<leader>aa',
         function()
           load_opencode(function() vim.cmd('Opencode') end)
         end,
-        desc = 'Opencode',
+        desc = 'Opencode toggle',
       },
     },
     opts = {
       preferred_picker = 'snacks',
+      keymap_prefix = '<leader>a',
       keymap = {
+        editor = {
+          -- ['<leader>aO'] = { 'open_output' },
+          ['<leader>am'] = { 'switch_mode' },
+          ['<leader>aR'] = { function() require('opencode.ui.ui').render_output(true) end },
+          ['<leader>aa'] = { 'toggle' },
+        },
         output_window = {
           ['<esc>'] = false,
         },
@@ -73,18 +81,34 @@ return {
           },
         },
       },
+      debug = {
+        enabled = true,
+        capture_streamed_events = true,
+      },
     },
-    config = function(_, opts)
-      local ok, opencode = pcall(require, 'opencode')
-      if not ok then return end
-      opencode.setup(opts)
+    -- config = function(_, opts)
+    --   local ok, opencode = pcall(require, 'opencode')
+    --   if not ok then return end
+    --   opencode.setup(opts)
+    --
+    --   -- local map = vim.keymap.set
+    --   -- local api = require('opencode.api')
+    --   -- map('n', '<leader>aa', api.switch_mode, { desc = 'Opencode: toggle agent/build' })
+    --   -- map('n', '<leader>as', api.select_session, { desc = 'Opencode: select session' })
+    --   -- map('n', '<leader>ap', api.configure_provider, { desc = 'Opencode: configure provider' })
+    --   -- map('n', '<leader>aR', function() require('opencode.ui.ui').render_output(true) end, { desc = 'Opencode full reload' })
+    -- end,
 
-      local map = vim.keymap.set
-      local api = require('opencode.api')
-      map('n', '<leader>aa', api.switch_mode, { desc = 'Opencode: toggle agent/build' })
-      map('n', '<leader>as', api.select_session, { desc = 'Opencode: select session' })
-      map('n', '<leader>ap', api.configure_provider, { desc = 'Opencode: configure provider' })
-    end,
+    vim.api.nvim_create_user_command('OpencodeReplay', function()
+      vim.opt.runtimepath:append('.')
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.notify = function(msg, level, opts)
+        level = level or vim.log.levels.INFO
+        opts = opts or {}
+        vim.api.nvim_echo({ { msg } }, true, opts)
+      end
+      require('tests.manual.streaming_renderer_replay').start({ set_statuscolumn = false })
+    end, {}),
   },
   {
     'nvim-lualine/lualine.nvim',
@@ -94,5 +118,24 @@ return {
       table.insert(opts.options.ignore_focus, 'opencode')
       table.insert(opts.options.ignore_focus, 'opencode_output')
     end,
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    optional = true,
+    ft = { 'opencode_output' },
+    opts = {
+      preview = {
+        filetypes = { 'opencode_output' },
+      },
+    },
+    opts_extend = { 'preview.filetypes' },
+  },
+  {
+    'luukvbaal/statuscol.nvim',
+    event = { 'BufNewFile', 'BufReadPost' },
+    opts = {
+      ft_ignore = { 'opencode_output' },
+    },
+    opts_extend = { 'ft_ignore' },
   },
 }
