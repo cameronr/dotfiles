d23b088a067b4c3f49ecb86b186c930c02f47ad5
Cameron Ring <cameron@cs.stanford.edu>
2026-08-05 14:17:12 -0700

    feat(opencode): add vim bindings

--- tmux/tmux-tokyo-night-theme-cam.sh
+++ tmux/tmux-tokyo-night-theme-cam.sh
@@ -68,7 +68,7 @@ function generate_inactive_window_string() {
     local error_icon
     error_icon=$(get_tmux_option "@theme_agent_error_icon" "")
     local agent_icon="#{?#{==:#{@agent_status},working},#[fg=${PALLETE[yellow]}]${working_icon} ,}#{?#{==:#{@agent_status},waiting},#[fg=${PALLETE[blue]}]${waiting_icon} ,}#{?#{==:#{@agent_status},idle},#[fg=${PALLETE[green]}]${idle_icon} ,}#{?#{==:#{@agent_status},error},#[fg=${PALLETE[red]}]${error_icon} ,}"
-    echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[fg_dark]}] #{?window_zoomed_flag,$zoomed_window_icon,}#{=/20/…:pane_title} ${agent_icon}${separator_end}"
+    echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[fg_dark]}] #{?window_zoomed_flag,$zoomed_window_icon,}#{=/15/…:pane_title} ${agent_icon}${separator_end}"
 }
 
 function generate_active_window_string() {
@@ -90,7 +90,7 @@ function generate_active_window_string() {
     fi
 
     # echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}#W #{?pane_synchronized,$pane_synchronized_icon,}${separator_end}#[none]"
-    echo "${separator_start}#[fg=${PALLETE[white2]}]#I${separator_internal}#[fg=${PALLETE[white2]}] #{?window_zoomed_flag,$zoomed_window_icon,}#{=/20/…:pane_title} #{?pane_synchronized,$pane_synchronized_icon,}${separator_end}#[none]"
+    echo "${separator_start}#[fg=${PALLETE[white2]}]#I${separator_internal}#[fg=${PALLETE[white2]}] #{?window_zoomed_flag,$zoomed_window_icon,}#{=/15/…:pane_title} #{?pane_synchronized,$pane_synchronized_icon,}${separator_end}#[none]"
 }
 
 export PALLETE
